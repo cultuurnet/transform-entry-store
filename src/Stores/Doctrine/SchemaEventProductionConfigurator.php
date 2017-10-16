bd9f4f2dc223c7b4ee8760addb0c89c4e4c5a2f3
Jonas Verhaeghe <jonas@localhost.localdomain>
2017-10-16 13:48:09 +0200

    IMP-56

---
+++ src/Stores/Doctrine/SchemaEventProductionConfigurator.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CultuurNet\TransformEntryStore\Stores\Doctrine;
+
+use CultuurNet\TransformEntryStore\Doctrine\DBAL\SchemaConfiguratorInterface;
+use Doctrine\DBAL\Schema\AbstractSchemaManager;
+use Doctrine\DBAL\Types\Type;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class SchemaEventProductionConfigurator implements SchemaConfiguratorInterface
+{
+    const EXTERNAL_ID_COLUMN = 'external_id';
+    
+    /**
+     * @param StringLiteral $tableName
+     */
+    public function __construct(StringLiteral $tableName)
+    {
+        $this->tableName = $tableName;
+    }
+    
+     /**
+     * @inheritdoc
+     */
+    public function configure(AbstractSchemaManager $schemaManager)
+    {
+        $schema = $schemaManager->createSchema();
+        $table = $schema->createTable($this->tableName->toNative());
+
+        $table->addColumn(self::EXTERNAL_ID_COLUMN, Type::STRING)
+            ->setLength(128)
+            ->setNotnull(true);
+        $table->addColumn(self::LOCATION_CDBID_COLUMN, Type::GUID)
+            ->setLength(36)
+            ->setNotnull(true);
+
+        $table->addUniqueIndex([self::EXTERNAL_ID_COLUMN]);
+
+        $schemaManager->createTable($table);
+    }
+}
